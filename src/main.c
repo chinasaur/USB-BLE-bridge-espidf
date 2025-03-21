e79fa6cf846982cb0afcf89879e1d5e82469b106
K6PLI <k6pli@arrl.net>
2025-03-21 15:28:51 -0700

    Add option to buffer incoming USB data to EOM.

--- src/main.c
+++ src/main.c
@@ -9,23 +9,49 @@
  */
 
 #include "esp_log.h"
-#include "usb.h"
+#include "freertos/ringbuf.h"
 #include "ble.h"
+#include "usb.h"
+
+#define BUFFER_SIZE (4096)
 
 int bridge_ble_data_to_usb(const uint8_t* data, size_t data_len) {
   usb_tx_blocking_if_connected(data, data_len, /*timeout_ms=*/1000);
   return 0;  // No error.
 }
 
-bool bridge_usb_data_to_ble(
+// Simply copies data from USB to BLE as it comes in.
+bool bridge_usb_data_to_ble_direct(
     const uint8_t* data, size_t data_len, void* unused_arg) {
   ble_write_and_notify_subscribed_clients(data, data_len);
   return true;  // Data consumed.
 }
 
+// Buffers incoming USB data until the end of a message (a semicolon) is 
+// detected, and then sends the complete message to BLE.
+bool bridge_usb_data_to_ble_buffer_to_end_of_message(
+    const uint8_t* data, size_t data_len, void* unused_arg) {
+  static uint8_t buffer[BUFFER_SIZE];
+  static int buffer_index = 0;
+  for (int i = 0; i < data_len; ++i) {
+    buffer[buffer_index++] = data[i];
+    if (data[i] == ';' || buffer_index == BUFFER_SIZE) {
+      ble_write_and_notify_subscribed_clients(buffer, buffer_index);
+      buffer_index = 0;
+    }
+  }
+  return true;  // Data consumed.
+}
+
 void app_main() {
-  usb_setup();
   ble_setup();
-  usb_register_new_data_receive_callback(bridge_usb_data_to_ble);
+  usb_setup();
+
   ble_register_new_data_receive_callback(bridge_ble_data_to_usb);
+
+  // Pick either one of these callbacks.  Buffering is a bit nicer for human
+  // readability but otherwise either should be fine.
+  // usb_register_new_data_receive_callback(bridge_usb_data_to_ble_direct)
+  usb_register_new_data_receive_callback(
+      bridge_usb_data_to_ble_buffer_to_end_of_message);
 }
